ae4ba772dbec7d49d5e4f08fed3177481bd888ce
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-09-29 19:50:26 -0600

    added initial relationships through model index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+})
 
 module.exports = {
   Product,
